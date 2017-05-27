d2cfdd8dabe1568ca7761efd95fdedc15a9d8b50
Slawomir Osiak <slawek-osiak@o2.pl>
2017-05-27 23:13:52 +0200

    add repo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-var name = prompt('Enter Your Name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a = prompt("Enter a");
+var h = prompt("Enter h");
+var triangleArea = a * h / 2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
